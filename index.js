504be46a96a21d8bb6dbe7ace6cf962f6e5a4dbc
alex <alex.rodritos.com>
2023-11-29 19:27:20 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const { PrismaClient } = require('@prisma/client');
+const qrcode = require('qrcode-terminal');
+
+const prisma = new PrismaClient();
+
+const client = new Client({
+    authStrategy: new LocalAuth({ clientId: "client-one" })
+});
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Contador de bosta online!');
+});
+
+client.on('message', async (message) => {
+    if (message.author == '556392084934@c.us' && message.body === 'reset') {
+        await prisma.merdas.deleteMany();
+    }
+
+    if (message.body.toLowerCase().includes('caguei')) {
+        const existingEntry = await prisma.merdas.findFirst({
+            where: { memberId: message.from }
+        });
+
+        let poopEmojis = '';
+
+        if (existingEntry) {
+            await prisma.merdas.update({
+                where: { id: existingEntry.id },
+                data: { count: existingEntry.count + 1 }
+            });
+
+            poopEmojis = '💩'.repeat(existingEntry.count + 1);
+        } else {
+            const contato = await message.getContact();
+            const userName = contato.shortName || contato.pushname || 'Usuário Desconhecido';
+
+            await prisma.merdas.create({
+                data: { userName, memberId: message.from, count: 1 }
+            });
+
+            poopEmojis = '💩';
+        }
+
+        const contato = await message.getContact();
+        const userName = contato.shortName || contato.pushname || 'Usuário Desconhecido';
+
+        client.sendMessage(message.from, `💩 Membro ${userName} cagou! 🔥🎉 Contagem atual: ${existingEntry ? existingEntry.count + 1 : 1} ${poopEmojis}`);
+    } else if (message.body.toLowerCase().includes('merda')) {
+        const podium = await prisma.merdas.findMany({
+            where: { memberId: message.from },
+            orderBy: { count: 'desc' },
+            take: 3,
+        });
+
+        let podiumMessage = '🏆 Pódio dos Cagadores 🏆\n';
+
+        podium.forEach((entry, index) => {
+            const contato = `Membro ${index + 1}: ${entry.userName || 'Usuário Desconhecido'}`;
+            const count = `Contagem: ${entry.count} 💩`;
+            podiumMessage += `${contato}\n${count}\n\n`;
+        });
+
+        client.sendMessage(message.from, podiumMessage);
+    }
+});
+
+client.initialize();
+
